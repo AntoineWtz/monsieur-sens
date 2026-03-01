5c1c198a5ef9f15b4fc691aac8a3088ad0ef6bc5
AntoineWtz <awtz.dev@gmail.com>
2026-03-01 15:10:15 +0100

    Mise à jour de la configuration Tailwind CSS avec ajout de nouvelles polices et ajustement des styles. Modification des titres et textes dans les sections ACF pour une meilleure présentation.

--- web/app/themes/monsieur-sens/tailwind.config.js
+++ web/app/themes/monsieur-sens/tailwind.config.js
@@ -1,24 +1,54 @@
 /** @type {import('tailwindcss').Config} config */
+import plugin from "tailwindcss/plugin";
 const config = {
-    content: [
-        "./views/**/*.twig", 
-        "./static/**/*.js"
+  content: ["./views/**/*.twig", "./static/**/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        "ms-black": "#0E0E0E",
+        "ms-light-black": "#262626",
+        "ms-white": "#F5F5F5",
+        "ms-gold": "#F5CB5C",
+      },
+      letterSpacing: {
+        display: "0.01em",
+      },
+      lineHeight: {
+        display: "1.5",
+      },
+      fontFamily: {
+        sans: [
+          "Manrope",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "Roboto",
+          "Noto Sans",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
         ],
-    theme: {
-        extend: {
-            colors: {
-                'ms-black': '#0E0E0E',
-                'ms-light-black': '#262626',
-                'ms-white': '#F5F5F5',
-                'ms-gold': '#F5CB5C',
-            },
-            fontFamily: {
-                sans: ['Manrope', 'ui-sans-serif', 'system-ui', '-apple-system', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
-                display: ['"Playwrite New Zealand Basic"', 'serif'],
-            },
-        },
+        display: ["Gabriela", "serif"],
+      },
     },
-    plugins: [],
+  },
+  plugins: [
+    plugin(({ addUtilities, theme }) => {
+      addUtilities(
+        {
+          ".font-display": {
+            letterSpacing: `${theme("letterSpacing.display")} !important`,
+            lineHeight: `${theme("lineHeight.display")} !important`,
+          },
+        },
+        {
+          variants: ["responsive"],
+        },
+      );
+    }),
+  ],
 };
 
 export default config;
